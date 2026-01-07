86b8ec5b0756b03585cc1d8e24c0995413273ff6
nekoma <pqh.one@gmail.com>
2026-01-07 19:13:41 +0700

    edit: browser

---
+++ home/programs/browser/default.nix
@@ -0,0 +1,27 @@
+{pkgs, ...}: {
+  programs.firefox = {
+    enable = true;
+    profiles.default = {
+      id = 0;
+      name = "default";
+
+      settings = {
+        "browser.privatebrowsing.autostart" = true;
+        "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "svg.context-properties.content.enabled" = true;
+        "browser.download.useDownloadDir" = false;
+        "datareporting.healthreport.uploadEnable" = false;
+        "datareporting.usage.uploadEnabled" = false;
+      };
+
+      extensions.packages = with inputs.firefox-addons.packages.${pkgs.system}; [
+        translate-web-pages
+        vimium
+      ];
+
+      userChrome = builtins.readFile ./userChrome.css;
+      userContent = builtins.readFile ./userContent.css;
+    };
+  };
+}
