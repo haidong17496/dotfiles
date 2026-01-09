b673a3297b2b201487a788abef06bbffa22abe2d
nekoma <pqh.one@gmail.com>
2026-01-09 13:45:44 +0700

    change: eww

--- home/default.nix
+++ home/default.nix
@@ -1,22 +1,49 @@
-{ config, pkgs, ... }:
-
 {
-    imports = [
-        ./hyprland/default.nix
-        ./walker/default.nix
-        ./shell/default.nix
-        ./programs/default.nix
-    ];
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./hyprland/default.nix
+    ./walker/default.nix
+    ./shell/default.nix
+    ./programs/default.nix
+  ];
 
-    home.pointerCursor = {
-        gtk.enable = true;
-        package = pkgs.bibata-cursors;
-        name = "Bibata-Modern-Ice";
-        size = 24;
+  home.packages = with pkgs; [
+    papirus-icon-theme
+  ];
+
+  gtk = {
+    enable = true;
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
     };
+    theme = {
+      name = "Catppuccin-Mocha-Standard-Blue-Dark";
+      package = pkgs.catppuccin-gtk.override {
+        accents = ["blue"];
+        size = "standard";
+        tweaks = ["rimless" "black"];
+        variant = "mocha";
+      };
+    };
+    cursorTheme = {
+      name = "Bibata-Modern-Ice";
+      package = pkgs.bibata-cursors;
+    };
+  };
 
-    home.username = "nekoma";
-    home.homeDirectory = "/home/nekoma";
-    
-    home.stateVersion = "25.11";
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Ice";
+    size = 24;
+  };
+
+  home.username = "nekoma";
+  home.homeDirectory = "/home/nekoma";
+
+  home.stateVersion = "25.11";
 }
--- home/programs/default.nix
+++ home/programs/default.nix
@@ -2,7 +2,7 @@
   imports = [
     ./nvim
     ./browser
-    ./waybar
+    ./eww
     ./git.nix
     ./yazi.nix
     ./bluetooth.nix
@@ -10,15 +10,25 @@
   ];
 
   home.packages = with pkgs; [
-    ripgrep
-    fd
-    jq
-    p7zip
-    bluetuith
-    pulsemixer
-    brightnessctl
-    bottom
-    grim
-    slurp
+    # --- Eww Widget Dependencies ---
+    eww
+    socat # For Eww IPC
+    jq # For parsing JSON (weather/lyrics)
+    acpi # For battery status
+    playerctl # For Media Player control
+    pamixer # For Volume Slider logic
+    brightnessctl # For Brightness Slider logic
+
+    # --- System Utilities ---
+    libnotify # For 'notify-send' (Popups)
+    inotify-tools # For detecting file changes instantly
+    bluetuith # TUI for Bluetooth
+    ripgrep # Fast search
+    fd # Fast find
+    p7zip # Archives
+    bottom # System monitor (btm)
+    grim # Screenshots
+    slurp # Area selection
+    pulsemixer # Audio TUI
   ];
 }
